4174e9e72b64913861c2d44e39e6f131fa888b66
Elcess <kelcess@alum.mit.edu>
2018-12-20 16:38:56 -0500

    install express-session; set up sequelize session store; sync store db

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const db = require('../db/db.js');
+
+// configure and create our database store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+// sync so that our session table gets created
+dbStore.sync();
 
 // you'll of course want static middleware so your browser can request things like your 'bundle.js'
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -11,6 +20,14 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // api routes
 app.use('/api', require('./apiRoutes')); // matches all requests to /api
